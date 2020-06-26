a54386fe486a63bd28a76cfea5183a63211ea2f1
Julia Bellamy <julia.bellamy@hws.edu>
2020-06-26 05:19:22 -0400

    autocorrelate and rolling window

--- test_acf.py
+++ test_acf.py
@@ -31,6 +31,30 @@ def corr_coef(x, y, rowvar=True):
     return c
 
 
+def acorr(x):
+
+    # Make sure data is a numpy array and make copy
+    x = np.asarray(x)
+    y = np.copy(x)
+
+    n = len(x)
+    lags = np.arange(0, n, 1)
+    acorr = []
+
+    # Calculate autocorrelation
+    for lag in lags:
+        if lag == 0:
+            c = (x * y).sum()
+            acorr.append(c)
+        else:
+            x = x[1:]
+            y = y[:n-lag]
+            c = (x * y).sum()
+            acorr.append(c)
+
+    return acorr
+
+
 def autocorrelation(x):
 
     # Determine the length of the data
@@ -74,6 +98,36 @@ def psd_from_autocorrolate(x, NFFT, Fs):
     return Pxx, fr
 
 
+def rolling_windows(x, n, noverlap=None, axis=0):
+    # Get all windows of x with length n as a single array, using strides to avoid data duplication.
+    # This was taken from the documentation
+
+    if noverlap is None:
+        noverlap = 0
+    x = np.asarray(x)
+
+    if n == 1 and noverlap == 0:
+        if axis == 0:
+            # This adds one more dimension
+            return x[np.newaxis]
+
+        else:
+            return x[np.newaxis].transpose()
+
+    noverlap = int(noverlap)
+    n = int(n)
+    step = n - noverlap
+
+    if axis == 0:
+        shape = (n, (x.shape[-1]-noverlap)//step)
+        strides = (x.strides[0], step * x.strides[0])
+    else:
+        shape = ((x.shape[-1] - noverlap) // step, n)
+        strides = (step * x.strides[0], x.strides[0])
+    return np.lib.stride_tricks.as_strided(x, shape=shape, strides=strides)
+
+
+'''''
 # Number of data points
 N = 512
 # time resolution
@@ -85,6 +139,7 @@ f1 = 20
 h = np.sin(f1 * 2.0*np.pi*t)
 
 fs = 100
+
 NFFT = fs
 pxx, freq = mlab.psd(h, NFFT=NFFT, Fs=fs)       # Works!
 P, f = psd_from_autocorrolate(h, NFFT=NFFT, Fs=fs)
@@ -102,6 +157,34 @@ ratio = pxx / P
 print(np.max(ratio))
 diff = P - pxx
 print(np.max(diff))
+'''''
+
+t = np.arange(1, 51, 1)
+f = 5
+sin = np.sin(f * 2 * np.pi * t)
+
+a = rolling_windows(t, 10, 2)
+print(a)
+print(a.shape)
+
+i = 0
+rxx = []
+n, m = a.shape
+while i < m:
+    c = a[:, i]
+    cor = acorr(c)
+    rxx.append(cor)
+    i += 1
+rxx = np.asarray(rxx)
+print(rxx.shape)
+print(rxx)
+
+
+
+
+
+
+
 
 
 
